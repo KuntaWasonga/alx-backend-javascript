05cf7f84c191e9e5eb70fa14b60add4138b3d743
KuntaWasonga <wkunta@rocketmail.com>
2023-03-23 11:08:37 +0300

    Lint correction

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,10 +4,10 @@ export default async function asyncUploadUser() {
   let res = {};
 
   try {
-    const photo =await uploadPhoto();
+    const photo = await uploadPhoto();
     const user = await createUser();
     res = { photo, user };
-  } catch(err) {
+  } catch (err) {
     res = { photo: null, user: null };
   }
   return res;
